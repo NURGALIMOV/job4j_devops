419d3e98700d131d16b44e8cc3ee109f7d5ea314
Ilshat Nurgalimov <il.nur86@mail.ru>
2025-11-24 09:57:20 +0300

    add listener test

---
+++ src/integrationTest/java/ru/job4j/devops/listener/CalcEventListenerTest.java
@@ -0,0 +1,89 @@
+package ru.job4j.devops.listener;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.utility.DockerImageName;
+import ru.job4j.devops.models.CalcEvent;
+import ru.job4j.devops.models.User;
+import ru.job4j.devops.repository.CalcEventRepository;
+import ru.job4j.devops.repository.UserRepository;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
+@SpringBootTest
+class CalcEventListenerTest {
+    private static final PostgreSQLContainer<?> POSTGRES = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    private static final KafkaContainer KAFKA = new KafkaContainer(
+            DockerImageName.parse("apache/kafka:3.7.2")
+    );
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    @Autowired
+    private CalcEventRepository calcEventRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeAll
+    static void beforeAll() {
+        POSTGRES.start();
+        KAFKA.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        POSTGRES.stop();
+        KAFKA.stop();
+    }
+
+    @DynamicPropertySource
+    public static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.kafka.consumer.auto-offset-reset", () -> "earliest");
+        registry.add("spring.kafka.bootstrap-servers", KAFKA::getBootstrapServers);
+        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+        registry.add("spring.datasource.username", POSTGRES::getUsername);
+        registry.add("spring.datasource.password", POSTGRES::getPassword);
+    }
+
+    @Test
+    void whenSendCalcEvent() {
+        var user = new User();
+        user.setName("Job4j new member : " + System.nanoTime());
+        User savedUser = userRepository.save(user);
+
+        CalcEvent calcEvent = new CalcEvent();
+        calcEvent.setUserId(savedUser.getId());
+        calcEvent.setFirst(1);
+        calcEvent.setSecond(2);
+        calcEvent.setResult(3);
+        calcEvent.setCreateDate(LocalDateTime.now());
+        calcEvent.setType("ADD");
+
+        kafkaTemplate.send("calc", calcEvent);
+        await()
+                .pollInterval(Duration.ofSeconds(3))
+                .atMost(10, SECONDS)
+                .untilAsserted(() -> {
+                    var optionalUser = calcEventRepository.findByUserId(savedUser.getId());
+                    assertThat(optionalUser).isPresent();
+                });
+    }
+}
